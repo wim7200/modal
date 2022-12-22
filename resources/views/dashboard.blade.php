0cab335247da83140b40cb105baf8539efbc4f5d
wim7200 <37835716+wim7200@users.noreply.github.com>
2022-12-22 21:29:03 +0100

    dashboard met Quote

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,9 +6,9 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-4xl h-screen place-items-center ">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-
+                {!!\Illuminate\Foundation\Inspiring::quote()!!}
             </div>
         </div>
     </div>
