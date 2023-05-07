2f84ba802a367ee9893f9fa733e4867393fe34cf
wim7200 <37835716+wim7200@users.noreply.github.com>
2023-05-07 13:10:29 +0200

    app down changed to normal

--- resources/views/errors/503_0.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('errors::minimal')
-
-@section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __('We are very sorry, but we are working in the shop! Back in no time...'))
