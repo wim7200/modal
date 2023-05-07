80f0628d5d94f9d4538ef58ef3deab0346f7109d
wim7200 <37835716+wim7200@users.noreply.github.com>
2023-05-07 13:16:59 +0200

    app down changed to normal

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -3,6 +3,6 @@
 @section('title', __('Service Unavailable'))
 @section('code', '503')
 @section('message', __('We will be down for a short time...
-                            </br>For MAINTENANCE please com back later... '))
+                            For MAINTENANCE please com back later... '))
 
 
