0eeacde3113618b140083439e679cdd119324c79
wim7200 <37835716+wim7200@users.noreply.github.com>
2023-05-07 13:25:11 +0200

    app down changed to normal

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,8 +1,18 @@
-@extends('errors::minimal')
+<!doctype html>
+<title>Site Maintenance</title>
+<style>
+    body { text-align: center; padding: 150px; }
+    h1 { font-size: 50px; }
+    body { font: 20px Helvetica, sans-serif; color: #333; }
+    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+    a { color: #dc8100; text-decoration: none; }
+    a:hover { color: #333; text-decoration: none; }
+</style>
 
-@section('title', __('Service Unavailable'))
-@section('code', '503')
-@section('message', __('We will be down for a short time...
-                            For MAINTENANCE please com back later... '))
-
-
+<article>
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+        <p>&mdash; The Team</p>
+    </div>
+</article>
