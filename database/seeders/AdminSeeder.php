d2fc9ec1a18d20c3d5b9718bbab7f43e7295e6ba
wim7200 <37835716+wim7200@users.noreply.github.com>
2023-05-21 11:22:19 +0200

    *Super-User met gescheiden roles

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -19,14 +19,23 @@ class AdminSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'admin',
-            'email' => 'admin@admin.com',
+            'name' => 'Super-Admin',
+            'email' => 'sad@admin.com',
             'email_verified_at' => now(),
             'profile_photo_path'=>'profile-photos/hert.jpg',
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'approved_at'=>now(),
             'approved_by'=>'Wim Torfs',
         ])->assignRole(['super-admin']);
+
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'approved_at'=>now(),
+            'approved_by'=>'Wim Torfs',
+        ])->assignRole('admin');
 
         User::create([
             'name' => 'manager',
