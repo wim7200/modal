dd5632acc72d2ef3b7eeba8c907b7f844ad7c1e1
wim7200 <37835716+wim7200@users.noreply.github.com>
2023-05-25 21:22:23 +0200

    foutje in kind create

--- resources/views/livewire/kind/kind-create.blade.php
+++ resources/views/livewire/kind/kind-create.blade.php
@@ -8,7 +8,7 @@
             <div class="mb-4">
                 <label for="exampleFormControlInput1"
                        class="block text-gray-700 text-sm font-bold mb-2">name</label>
-                <input type="textarea"
+                <input type="text"
                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                        id="exampleFormControlInput1" placeholder="Enter Name" wire:model="name">
                 @error('name') <span class="text-red-500">{{ $message }}</span>@enderror
