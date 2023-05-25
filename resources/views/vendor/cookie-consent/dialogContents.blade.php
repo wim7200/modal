a7364724d5e760fb59bd1d65ce3bb4b405fd3928
wim7200 <37835716+wim7200@users.noreply.github.com>
2023-05-25 21:03:42 +0200

    cookies color green in local Vendor

--- resources/views/vendor/cookie-consent/dialogContents.blade.php
+++ resources/views/vendor/cookie-consent/dialogContents.blade.php
@@ -12,7 +12,7 @@
                 <div class="mt-2 flex-shrink-0 w-full sm:mt-0 sm:w-auto">
                     <button class="js-cookie-consent-agree cookie-consent__agree cursor-pointer
                                     flex items-center justify-center px-4 py-2 rounded-md
-                                    text-sm font-medium text-yellow-800 bg-green-400 hover:bg-green-300">
+                                    text-sm font-medium text-yellow-800 bg-green-600 hover:bg-green-300">
                         {{ trans('cookie-consent::texts.agree') }}
                     </button>
                 </div>
